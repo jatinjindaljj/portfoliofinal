8dfa5f08896ac438d2cca97ca428e001fb7468aa
Jatin Jindal <reachtojatin1@gmail.com>
2022-05-21 11:27:08 +0530

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+
+
+
+window.addEventListener("scroll",()=>{
+    console.log(window.pageYOffset);
+    if(window.pageYOffset>20){
+        document.getElementById("header").style.boxShadow="0 0 0.625rem black";
+    }else{
+        document.getElementById("header").style.boxShadow="none";
+
+    }
+})
